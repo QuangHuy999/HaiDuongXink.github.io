2b1734b969d6e58c2e9c19e5b9cc799468843666
Quang Huy <huylequang1525@gmail.com>
2022-11-27 00:16:23 +0700

    first commit

--- js/myJs.js
+++ js/myJs.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/Chocon.jpg",
+      imageUrl: "img/cuteCat.jpg",
       imageWidth: 200,
       imageHeight: 150,
       background: '#fff url("img/iput-bg.jpg")',
